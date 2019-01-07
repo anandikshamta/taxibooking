3c5e05dbe7be3a7cfc3e5c56d9e1c30afde8bf8f
sankaran.thiyagarajan <sankaran.thiyagarajan@balfour.com>
2019-01-08 01:56:58 +0530

    added table column, fixed issues

--- wp-content/plugins/cab_booking/book_here/book_here.php
+++ wp-content/plugins/cab_booking/book_here/book_here.php
@@ -27,8 +27,7 @@ class BookHere
 		$return_date = date("Y-m-d H:i:s",strtotime($param['return_date']." ".$param['return_date_hours'].":".$param['return_date_mins']));
 		$created_at = date("Y-m-d H:i:s");
 
-		$insquery = "insert into `wp_cab_booking`(`company_id`,`from_address`,`to_address`,`extra`,`passenger`,`luggage`,`way`,`pickup_date`,`return_date`,`meet_greet`,`baby_seat`,`booster`,`wheelcair`,`created_date`) values
-					('".$company_id."','".$param['from']."','".$param['to']."','".$param['extra']."','".$param['passengers']."','".$param['luggage']."','".$param['way']."','".$pickup_date."','".$return_date."','".$param['meet_greet']."','".$param['baby_seat']."','".$param['booster_seat']."','".$param['wheel_chair']."','".$param['promo_code']."','".$created_at."')";
+		$insquery = "insert into `wp_cab_booking`(`company_id`,`from_address`,`to_address`,`extra`,`passenger`,`luggage`,`way`,`pickup_date`,`return_date`,`meet_greet`,`baby_seat`,`booster`,`wheelcair`,`promo_code`,`created_date`) values ('".$company_id."','".$param['from']."','".$param['to']."','".$param['extra']."','".$param['passengers']."','".$param['luggage']."','".$param['way']."','".$pickup_date."','".$return_date."','".$param['meet_greet']."','".$param['baby_seat']."','".$param['booster_seat']."','".$param['wheel_chair']."','".$param['promo_code']."','".$created_at."')";
 
 		$wpdb->query($insquery);
 		$insert_id = $wpdb->insert_id;
@@ -50,22 +49,21 @@ class BookHere
 		global $wpdb;
 		$arr = array();
 		$param = $_POST['frmdata'];
-		$company = json_decode(CabDecrypt($_POST['ka']));
-		$company_id = $company->id;
-		$pickup_date = date("Y-m-d H:i:s",strtotime($param['pickup_date']." ".$param['pickup_date_hours'].":".$param['pickup_date_mins']));
-		$return_date = date("Y-m-d H:i:s",strtotime($param['return_date']." ".$param['return_date_hours'].":".$param['return_date_mins']));
-		$created_at = date("Y-m-d H:i:s");
+		//$company = json_decode(CabDecrypt($_POST['ka']));
+		//$company_id = $company->id;
 
-		$insquery = "insert into `wp_cab_booking`(`company_id`,`from_address`,`to_address`,`extra`,`passenger`,`luggage`,`way`,`pickup_date`,`return_date`,`meet_greet`,`baby_seat`,`booster`,`wheelcair`,`created_date`) values
-					('".$company_id."','".$param['from']."','".$param['to']."','".$param['extra']."','".$param['passengers']."','".$param['way']."','".$pickup_date."','".$return_date."','".$param['meet_greet']."','".$param['baby_seat']."','".$param['booster_seat']."','".$param['wheel_chair']."','".$param['promo_code']."','".$created_at."')";
-
-		$wpdb->query($insquery);
-		$insert_id = $wpdb->insert_id;
-		$arr['id'] = $company_id;
-		$arr['book_id'] = $insert_id;
+		if(!empty($param['bookingId'])) {
+			$wpdb->query("update wp_cab_booking set vehicle_id = '".$param['vehicleId']."' where id=".$param['bookingId']." limit 1;");
+		}
+		$arr['id'] = $param['bookingId'];
 		$data = json_encode($arr);
 		$dataenc = CabEncrypt($data);
-		echo "2##########".$dataenc;exit;
+		$responseData = array_merge($arr, $param);
+		$response = [
+			'data'	=> $responseData,
+			'dataenc' => $dataenc
+		];
+		echo json_encode($response); exit;
 	}
 
 	function RegisterStep3()
@@ -100,4 +98,3 @@ class BookHere
 		echo json_encode($response); exit;
 	}
 }
-?>
